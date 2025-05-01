380eda16749be4f3a88edc7ce309b7231afa9307
harshkumar05 <hk923458@gmail.com>
2025-05-02 00:26:32 +0530

    remote setup

---
+++ src/main/java/com/pp/luxecardenbe/LuxecardenbeApplication.java
@@ -0,0 +1,13 @@
+package com.pp.luxecardenbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LuxecardenbeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LuxecardenbeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pp/luxecardenbe/LuxecardenbeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pp.luxecardenbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LuxecardenbeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
